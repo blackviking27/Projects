ac0950d745ca52cc13a8f88944d8926094e976d8
Abhishek Prajapati <46470516+blackviking27@users.noreply.github.com>
2019-08-27 20:58:18 +0530

    Update hangman_text_game.py

--- hangman_text_game.py
+++ hangman_text_game.py
@@ -1,11 +1,12 @@
-from random_words import *
+from random_words import * 
+# The welcome screen is displayed with the help of this function
 def main():
     print("Welcome to Hangman Game")
     print("The rules are simple you have to guess the word")
     print("You are provided limited number of guesses i.e 3")
 
 # checking for a letter that is matching
-def check (n):
+def check (n,letter):
     temp=0
     for i in range(len(letter)):
         if letter[i] == n:
@@ -32,7 +33,7 @@ def complete():
 # generating a random word
 r = RandomWords()
 letter = r.random_word() # storing a random word in variable
-guess = 3
+guess = 3 # number of attempts the user has
 l = [] # creating a list to check if letter guessed is correct
 for i in range(len(letter)):
     l.append('*')
@@ -43,7 +44,7 @@ print("The length of the word is ",len(letter))
 # executing loop till the user has guesses left
 while guess>0:
     n=input("Guess a letter")
-    check(n)
+    check(n,letter)
     show()
     print()  # starting a new line
     print("The number of guesses left is ",guess)
