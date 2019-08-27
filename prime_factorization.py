e9068da5bc88e9bf63a6b9a646507fce10202277
Abhishek Prajapati <46470516+blackviking27@users.noreply.github.com>
2019-08-27 21:03:54 +0530

    Update prime_factorization.py

--- prime_factorization.py
+++ prime_factorization.py
@@ -1,15 +1,23 @@
+# Defining a function to check for a prime
+
 def prime(a):
     for i in range(2,a):
         if a % i == 0:
             return False
     return True
+
 list =[]
-n = int(input("Enter a to find its prime factor"))
+n = int(input("Enter a number to find its prime factor"))
+
+# Checking for prime factors of the given number by runnung a loop
 for i in range(1,n+1):
     if n % i == 0:
         if prime(i):
             list.append(i)
+
+# Printin gthe prime factors of the user input number            
 print("The prime factors of the given numbers is")
+    
 for i in list:
     if i != 1:
-        print(i)
+        print(i, end="")
